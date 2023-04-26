b1fb619160f9ab5ab762c3f8b643d1cd684187ae
diddyjax19 <adetobi59@gmail.com>
2023-04-26 20:06:58 +0000

    changes made

---
+++ game.py
@@ -0,0 +1,170 @@
+import math
+import time
+from time import sleep  # welcome message animation 
+import sys  # to access parameters and functions
+from player import HumanPlayer, SmartComputerPlayer
+
+
+# welcome title with animations
+welcome_message = "Welcome to My Ultimate Tic_Tac_Toe game!\n"
+
+for x in welcome_message:
+    print(x, end='')
+    sys.stdout.flush()
+    sleep(.1)
+# print game instructions
+
+
+game_instructions = '''
+
+Please read instructions carefully to play the game: \n
+- The game is displayed as a 3X3 grid
+- The user(you) will start the game and is denoted with the letter 'O'
+- The computer (opposition) is denoted by the letter 'X'
+- To place your letter type a number between 0-8
+- This will choose a position on the board.
+- You can win either horizontally, vertically or diagonally!
+- If all 8 spaces are full and no one has won,
+- The game  ends in a tie and there will be no winner
+- The first among the players who have 3 same symbols in a line.                   
+                           '''
+print(game_instructions)
+
+
+# Enter player's names
+def valid_name():
+    '''
+    Gets player name and only accept letters.
+    '''
+    print("What is your name?")
+    while True:
+        name = input("My name is: ")
+        if not name.isalpha():
+            print("Invalid Entry Enter only letters.")
+            continue
+
+        else:
+            print(f"Welcome {name}!")
+            break
+    return name
+
+
+valid_name()
+
+
+def start_game():
+    '''
+    asks the user to enter 's' so the game can start
+    '''
+    while True:
+        start_game_input = input("Type 'S' Start the game:\n").lower()
+        if start_game_input == 's':
+            game_starting = 'The Game is starting...'
+            print(game_starting, end="\r")
+            sleep(1)
+            print(" " * len(game_starting), end="\r")
+            sleep(1)
+            break
+        else:
+            print(f"{start_game_input}Invalid,press 'S' to start the game.")
+
+
+start_game()
+
+
+class TicTacToe():
+    def __init__(self):
+        self.board = self.make_board()
+        self.current_winner = None
+
+    @staticmethod
+    def make_board():
+        return [' ' for _ in range(9)]
+
+    def print_board(self):
+        for row in [self.board[i*3:(i+1) * 3] for i in range(3)]:
+            print('| ' + ' | '.join(row) + ' |')
+
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('| ' + ' | '.join(row) + ' |')
+
+    def make_move(self, square, letter):
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
+
+    def winner(self, square, letter):
+        # check the row
+        row_ind = math.floor(square / 3)
+        row = self.board[row_ind*3:(row_ind+1)*3]
+        # print('row', row)
+        if all([s == letter for s in row]):
+            return True
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        # print('col', column)
+        if all([s == letter for s in column]):
+            return True
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            # print('diag1', diagonal1)
+            if all([s == letter for s in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            # print('diag2', diagonal2)
+            if all([s == letter for s in diagonal2]):
+                return True
+        return False
+
+    def empty_squares(self):
+        return ' ' in self.board
+
+    def num_empty_squares(self):
+        return self.board.count(' ')
+
+    def available_moves(self):
+        return [i for i, x in enumerate(self.board) if x == " "]
+
+
+def play(game, x_player, o_player, print_game=True):
+
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X'
+    while game.empty_squares():
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+        if game.make_move(square, letter):
+
+            if print_game:
+                print(letter + ' makes a move to square {}'.format(square))
+                game.print_board()
+                print('')
+
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+                return letter  # ends the loop and exits the game
+            letter = 'O' if letter == 'X' else 'X'  # switches player
+
+        time.sleep(.8)
+
+    if print_game:
+        print('It\'s a tie!')
+
+
+if __name__ == '__main__':
+    x_player = SmartComputerPlayer('X')
+    o_player = HumanPlayer('O')
+    t = TicTacToe()
+    play(t, x_player, o_player, print_game=True)
